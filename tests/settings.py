844c8fd4a78ddc2aaa1b8224a9d5ad8864b6203b
George Marshall <george@georgemarshall.name>
2016-05-13 16:43:13 -0700

    Updated test settings

--- tests/settings.py
+++ tests/settings.py
@@ -1,13 +1,10 @@
-SECRET_KEY = 'dummy_key'
+SECRET_KEY = 'test_key'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:'
     }
 }
-
-MIDDLEWARE_CLASSES = []
 
 INSTALLED_APPS = [
     'composite_field',
